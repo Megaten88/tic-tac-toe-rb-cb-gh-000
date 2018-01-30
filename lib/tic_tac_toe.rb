47a33cbf3e2263c90efeedc39a8ed81438fb0005
Mayra  Salazar <mayrasalazar@unitec.edu>
2018-01-30 17:12:36 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -112,8 +112,8 @@ def play(board)
     over?(board)
   end
   if won?(board)
-    puts "Congratulations #{winner(board)!}"
+    puts "Congratulations #{winner(board)}!"
   elsif draw?(board)
-    puts "Cat's Game!"
+    puts "Cat\'s Game!"
   end
 end
