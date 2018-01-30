4b86b5edf88c7cf901204a4964a4f799b84f689b
Mayra  Salazar <mayrasalazar@unitec.edu>
2018-01-30 17:52:09 +0000

    automatically backed up by learn

--- spec/02_play_spec.rb
+++ spec/02_play_spec.rb
@@ -46,7 +46,7 @@ describe './lib/tic_tac_toe.rb' do
 
       play(board)
 
-      expect(board).to match_array(["X", "O", "X", " ", " ", " ", " ", " ", " "])
+      expect(board).to match_array(["X", "O", " ", " ", " ", " ", " ", " ", " "])
     end
 
     it 'checks if the game is won after every turn' do
