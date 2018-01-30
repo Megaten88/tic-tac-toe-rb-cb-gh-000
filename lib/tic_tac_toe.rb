1777022dd98262327d18d91c3461b236b3494143
Mayra  Salazar <mayrasalazar@unitec.edu>
2018-01-30 16:44:34 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -21,8 +21,8 @@ def input_to_index(input)
   input.to_i-1
 end
 
-def move(board,index,value="X")
-  
+def move(board, index, current_player = "X")
+  board[index] = current_player
 end
 
 WIN_COMBINATIONS = [
